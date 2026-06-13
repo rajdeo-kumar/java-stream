d5506a771baf944a83eb7493e635a19e1fc50040
rajdeo <rajdeo.rajdeo@gmail.com>
2026-06-13 23:55:05 +0530

    Refactor code as method specific.

--- src/main/java/com/ayberix/streams/ConvertListOfIntegerToSquare.java
+++ src/main/java/com/ayberix/streams/ConvertListOfIntegerToSquare.java
@@ -1,22 +1,30 @@
 package com.ayberix.streams;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class ConvertListOfIntegerToSquare {
     public static void main(String[] args) {
-        var numbers = java.util.List.of(1, 2, 3, 4, 5);
+        var numbers = List.of(1, 2, 3, 4, 5);
+        List<Integer> list = List.of(1,2,3,4,5, 12 ,7,9,15,88,20);
+        List<?> squares = findSquareOfNumber(numbers);
+        System.out.println("Squares: " + squares);
+
+        double avgOfSquareOfNumber = findAvgOfSquareOfNumber(list);
+        System.out.println("Avg Of SquareOfNumber: " + avgOfSquareOfNumber);
+    }
+
+    private static List<Integer> findSquareOfNumber(List<Integer> numbers) {
         var squares = numbers.stream()
                 .map(n -> n * n)
                 .toList();
-        System.out.println("Squares: " + squares);
+        return squares;
+    }
 
-        List<Integer> list = List.of(1,2,3,4,5, 12 ,7,9,15,88,20);
+    private static double findAvgOfSquareOfNumber(List<Integer> list) {
         double avgOfSquareOfNumber = list.stream()
                 .map(e -> e * e)
                 .filter(e -> e > 100)
                 .mapToInt(e -> e).average().getAsDouble();
-
-        System.out.println("Avg Of SquareOfNumber: " + avgOfSquareOfNumber);
+        return avgOfSquareOfNumber;
     }
 }
